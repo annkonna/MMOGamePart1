04903c4b80c6c410551c22907a5966e1e8badb06
agkon <annkonna@buffalo.edu>
2019-03-18 10:49:21 -0400

    initial project version

---
+++ welcome.js
@@ -0,0 +1,10 @@
+setInterval(function(){ updatePlayers(); }, 3000);
+
+function updatePlayers() {
+  document.getElementById("players").value = "Number of players: "+Math.floor((Math.random() * 100) + 1);
+}
+
+function redirect() {
+//    setTimeout('window.location.href="game4.html"', 0); 
+  window.location.href="game4.html"
+}
